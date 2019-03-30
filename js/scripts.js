50dfd4c3a9f00ee3d86ab04a98ffc58b710a9c69
Noel Bor <NoelkBor@gmail.com>
2019-03-30 15:05:13 +0300

    formula in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
   $( "input[type='submit']").click(function() {
   $( ".user" ).slideUp(3000);
   });
+
   $( "input[type='next']").click(function() {
   $( ".questions" ).slideUp();
 });
+
 });
