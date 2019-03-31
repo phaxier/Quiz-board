f52d1b93ed0e7dc3b08e6677d7fe6864dace7b59
Noel Bor <NoelkBor@gmail.com>
2019-04-01 00:00:08 +0300

    updated scipts.js file formula to add marks

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,14 @@ $(document).ready(function(){
   $("input[type='next']").click(function() {
   $( ".answers").toggle();
 });
+//$("questions").submit(function(event){
+//var d1=("input:radio[name=quiz1]:checked").val();
+//var d2=("input:radio[name=quiz2]:checked").val();
+//var d3=("input:radio[name=quiz3]:checked").val();
+//var d4=("input:radio[name=quiz4]:checked").val();
 
-var d1=("input:radio[name=quiz1]:checked").val();
-var d2=("input:radio[name=quiz2]:checked").val();
-var d3=("input:radio[name=quiz3]:checked").val();
-var d4=("input:radio[name=quiz4]:checked").val();
- function add(d1,d2,d3,d4){
-   var marks= d1+d2+d3+d4;
-   return marks;
- });
+//var marks=parseInt(d1)+parseInt(d2)+parseInt(d3)+parseInt(d4);//
+//$("marks").text(You Scored:"+result+*25");
+
+//});
 });
