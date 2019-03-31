4a8cac74338c8f5b71c30392cf4307d2c21494a0
Noel Bor <NoelkBor@gmail.com>
2019-03-31 23:41:20 +0300

    updated scipts.js file formula to add marks

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,13 @@ $(document).ready(function(){
   $("input[type='next']").click(function() {
   $( ".answers").toggle();
 });
- //$ ('#submit').click(function() {
-//$( "input[type='radio']").click(function()
- //$( ".questions" ).toggle();
-  // });
 
+var d1=("input:radio[name=quiz1]:checked").val();
+var d2=("input:radio[name=quiz2]:checked").val();
+var d3=("input:radio[name=quiz3]:checked").val();
+var d4=("input:radio[name=quiz4]:checked").val();
+ function add(d1,d2,d3,d4){
+   var marks= d1+d2+d3+d4;
+   return marks;
+ });
 });
