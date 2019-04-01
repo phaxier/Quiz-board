e66fedec1fa169e48df9238756c69eec9abb13cc
Noel Bor <NoelkBor@gmail.com>
2019-04-01 09:37:40 +0300

    update forms in the index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
   $("input[type='next']").click(function() {
   $( ".answers").toggle();
 });
-//$("questions").submit(function(event){
+//$("questions").submit(function(){
 //var d1=("input:radio[name=quiz1]:checked").val();
 //var d2=("input:radio[name=quiz2]:checked").val();
 //var d3=("input:radio[name=quiz3]:checked").val();
 //var d4=("input:radio[name=quiz4]:checked").val();
 
-//var marks=parseInt(d1)+parseInt(d2)+parseInt(d3)+parseInt(d4);//
-//$("marks").text(You Scored:"+result+*25");
+//var marks=parseInt(d1)+parseInt(d2)+parseInt(d3)+parseInt(d4);
+//$("marks").text(You Scored:"+marks+*25");
 
 //});
 });
